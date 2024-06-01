0b7f70567b521477f1e01cfc60f031ca6540fbcf
cntechpower <root@cntechpower.com>
2024-06-01 23:22:17 +0800

    feat: update async_task

--- async_task/async_task.go
+++ async_task/async_task.go
@@ -9,13 +9,13 @@ import (
 )
 
 // mux maps a type to a handler
-var mux = asynq.NewServeMux()
+var mux *asynq.ServeMux
 
 var srv *asynq.Server
 
 var cli *asynq.Client
 
-var logger *log.Header
+var logger = log.NewHeader("async_task")
 
 func RegisterTaskHandlerFunc(taskType string, handler func(context.Context, *asynq.Task) error) {
 	mux.HandleFunc(taskType, handler)
@@ -36,7 +36,12 @@ func AddTask(taskType string, payload []byte, opts ...asynq.Option) error {
 	return nil
 }
 
-func Init(redisUri string) error {
+func InitCli(redisUri string) {
+	cli = asynq.NewClient(asynq.RedisClientOpt{Addr: redisUri})
+}
+
+func InitWorker(redisUri string) error {
+	mux = asynq.NewServeMux()
 	srv = asynq.NewServer(
 		asynq.RedisClientOpt{Addr: redisUri},
 		asynq.Config{
@@ -51,9 +56,6 @@ func Init(redisUri string) error {
 			// See the godoc for other configuration options
 		},
 	)
-	cli = asynq.NewClient(asynq.RedisClientOpt{Addr: redisUri})
-
-	logger = log.NewHeader("async_task")
 	return srv.Start(mux)
 }
 
